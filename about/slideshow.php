9e970e8e8420d62862b620cdfcb8de48c54f0b5a
Cody Ray Hoeft <hoeftc@onid.oregonstate.edu>
2014-02-14 19:22:04 -0800

    Moved Table to new slideshow page

---
+++ about/slideshow.php
@@ -0,0 +1,49 @@
+<?php 
+    $pagetitle="How to Setup a Desktop Slideshow";
+    $root = "../"; 
+    include($root."images.php");
+    include($root."header.php");?>
+        <table class="aligncenter">
+            <caption>Consult the table below to find if your operating system allows you to setup a desktop slideshow</caption>
+            <thead>
+                  <tr>
+                          <th>Operating System</th>
+                          <th>Slideshow Compatible</th>
+                          <th>More Info</th>
+                  </tr>
+            </thead>
+            <tbody>
+                  <tr>
+                          <th>Windows (7/8)</th>
+                          <td>Yes</td>
+                          <td><a href="http://www.howtogeek.com/howto/6158/turn-your-desktop-into-a-picture-slideshow-in-windows-7/" title="How to turn you desktop into a picture slideshow in Windows 7">How-To Geek </a></td>
+                  </tr>
+                  <tr>
+                          <th>Mac OS (X)</th>
+                          <td>Yes</td>
+                          <td><a href="http://www.ehow.com/how_2181393_desktop-slideshow-mac-os-x.html" title="How to set-up a desktop slideshow on Mac OS X">eHow</a></td>
+                  </tr>  
+                  <tr>
+                          <th>Linux (Unity)</th>
+                          <td>Yes (need Wallch)</td>
+                          <td><a href="http://sourceforge.net/projects/wall-changer/" title="Wallch on Sourceforge">Wallch</a></td>
+                  </tr>
+                  <tr>
+                          <th>Chrome OS</th>
+                          <td>Not Really *</td>
+                          <td><a href="http://www.gottabemobile.com/2013/09/14/6-chromeos-tips-make-chromebook-sparkle/#attachment_203164" title="Surprise Me feature of Chrome OS">Surprise Me</a></td>
+                  </tr>
+                  <tr>
+                          <th>Other</th>
+                          <td colspan="2"><a href="http://people.oregonstate.edu/~hoeftc/epicphotos/about/contact.php">Let me know</a>, I will add to the list.</td>
+                  </tr>
+            </tbody>
+            <tfoot>
+                  <tr>
+                          <th colspan="3">* Chrome OS doesn't let you select your photos</th>
+                  </tr>
+            </tfoot>
+        </table>
+    <?php include($root."footer.php");
+
+?>
--- categories/index.php
+++ categories/index.php
@@ -9,38 +9,4 @@
     <p>Paragraph...</p>
 </aside>
 <p>Epic Photography provides a large collection of High Resolution Photos that look great in a desktop background slideshow. My site will provide images that have superior quality compared to others The Photos on this site have been handpicked by me for their beauty over the past five years. I only select natural images: scenic pictures, macro close up images, and Astronomy Pictures. But other people should have an epic background slideshow, this website will allow anyone to download the images and use them on their own machine</p>
-<table class="aligncenter">
-    <caption>This is a simple table caption</caption>
-    <thead>
-          <tr>
-                  <th>Header cell</th>
-                  <th>Header cell</th>
-                  <th>Header cell</th>
-          </tr>
-    </thead>
-    <tbody>
-          <tr>
-                  <th>Header cell</th>
-                  <td>Data cell</td>
-                  <td>Data cell</td>
-          </tr>
-          <tr>
-                  <th>Header cell</th>
-                  <td>Data cell</td>
-                  <td>Data cell</td>
-          </tr>  
-          <tr>
-                  <th>Header cell</th>
-                  <td>Data cell</td>
-                  <td>Data cell</td>
-          </tr>
-    </tbody>
-    <tfoot>
-          <tr>
-                  <th>Totals</th>
-                  <th>Header cell</th>
-                  <th>Header Cell</th>
-          </tr>
-    </tfoot>
-</table>
 <?php include '../footer.php'; ?>
